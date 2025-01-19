03ef7c64c3b30c29c713f8fa5fc9b1782ce777f9
jxffx <jx.aeptiktok@gmail.com>
2025-01-19 14:52:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import React from './react';
-import ReactDOM from './react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './styles.css';
-ReactDOM.render(React.createElement(React.StrictMode, null,
-    React.createElement(App, null)), document.getElementById('root'));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
