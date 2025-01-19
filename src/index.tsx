d3e209c2ced0538490e9213dad9a3760b23722ce
jxffx <jx.aeptiktok@gmail.com>
2025-01-19 14:13:03 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
-import './index.css'
-import React from "react";
-import { render } from "react-dom";
-import { App } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
