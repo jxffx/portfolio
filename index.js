ef729b4513aa70a4688b3d81973f977a0ee7d8f6
jxffx <jx.aeptiktok@gmail.com>
2025-01-19 15:03:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'https://cdn.skypack.dev/react';
+import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+
+import App from './App.js';
 import './styles.css';
 
 ReactDOM.render(
